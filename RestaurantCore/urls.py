03af77d85a958b27e765178df74376219ce99142
mkyw <michaelkwong03@gmail.com>
2024-06-12 18:57:53 -0500

    Redirect default landing page

--- RestaurantCore/urls.py
+++ RestaurantCore/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 from routers import router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    path('api/', include((router.urls, 'core_api'), namespace='core_api')),
-
+    path("api/", include((router.urls, "core_api"), namespace="core_api")),
+    path("", lambda request: HttpResponseRedirect("/api/menu/")),
 ]
